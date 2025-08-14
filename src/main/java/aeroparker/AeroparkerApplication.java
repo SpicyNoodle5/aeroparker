91e14a999df63dd3ba571c5ac1ccd1d151c96197
SpicyNoodle5 <93035068+SpicyNoodle5@users.noreply.github.com>
2025-08-14 12:08:38 +0100

    Setup

---
+++ src/main/java/aeroparker/AeroparkerApplication.java
@@ -0,0 +1,13 @@
+package aeroparker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroparkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroparkerApplication.class, args);
+	}
+
+}
